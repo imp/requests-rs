44e10c99758760c494e3baad405802d7fd7c3ea2
Cyril Plisko <cyril.plisko@mountall.com>
2016-01-25 22:19:16 +0200

    Add test for Response.reason

--- src/lib.rs
+++ src/lib.rs
@@ -10,29 +10,34 @@ mod test {
     fn test_get() {
         let res = requests::get("http://httpbin.org/get").unwrap();
         assert_eq!(res.status_code, 200);
+        assert_eq!(res.reason, "OK");
     }
 
     #[test]
     fn test_post() {
         let res = requests::post("http://httpbin.org/post").unwrap();
         assert_eq!(res.status_code, 200);
+        assert_eq!(res.reason, "OK");
     }
 
     #[test]
     fn test_put() {
         let res = requests::put("http://httpbin.org/put").unwrap();
         assert_eq!(res.status_code, 200);
+        assert_eq!(res.reason, "OK");
     }
 
     #[test]
     fn test_head() {
         let res = requests::head("http://httpbin.org/get").unwrap();
         assert_eq!(res.status_code, 200);
+        assert_eq!(res.reason, "OK");
     }
 
     #[test]
     fn test_detele() {
         let res = requests::delete("http://httpbin.org/delete").unwrap();
         assert_eq!(res.status_code, 200);
+        assert_eq!(res.reason, "OK");
     }
 }
