27f56e00076ec3e7f79e8a673b5ec8a25916edef
Cyril Plisko <cyril.plisko@mountall.com>
2016-01-26 14:49:09 +0200

    Add text field

--- src/requests/response.rs
+++ src/requests/response.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 use hyper;
 
 pub type HyperResponse = hyper::client::response::Response;
@@ -9,11 +10,12 @@ pub struct Response {
     pub reason: String,
     pub ok: bool,
     pub content: Vec<u8>,
+    pub text: String,
     raw: HyperResponse,
 }
 
 impl Response {
-    pub fn from_hyper(raw: HyperResponse) -> Self {
+    pub fn from_hyper(mut raw: HyperResponse) -> Self {
         let status_code;
         let reason;
         {
@@ -21,12 +23,16 @@ impl Response {
             status_code = sc;
             reason = r.to_string();
         }
+        let mut text = String::new();
+        raw.read_to_string(&mut text).unwrap();
+
         Response {
             url: raw.url.serialize(),
             status_code: status_code,
             reason: reason,
             ok: status_code == 200,
             content: vec![],
+            text: text,
             raw: raw,
         }
     }
