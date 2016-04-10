06deb0c042988f8667390fd97440814b4dc03ca8
Cyril Plisko <cyril.plisko@mountall.com>
2016-04-10 17:45:45 +0300

    Update default UserAgent

--- src/requests/request.rs
+++ src/requests/request.rs
@@ -3,12 +3,16 @@ use hyper::header::UserAgent;
 use super::response::Response;
 use super::Result;
 
-const REQUESTS_USER_AGENT: &'static str = "requests-rs/0.0.0";
+const DEFAULT_USER_AGENT: &'static str = concat!("requests-rs/", env!("CARGO_PKG_VERSION"));
+
+fn default_user_agent() -> UserAgent {
+    UserAgent(DEFAULT_USER_AGENT.to_owned())
+}
 
 pub fn get(url: &str) -> Result<Response> {
     hyper::Client::new()
         .get(url)
-        .header(UserAgent(REQUESTS_USER_AGENT.to_owned()))
+        .header(default_user_agent())
         .send()
         .map(Response::from)
 }
@@ -16,7 +20,7 @@ pub fn get(url: &str) -> Result<Response> {
 pub fn post(url: &str) -> Result<Response> {
     hyper::Client::new()
         .post(url)
-        .header(UserAgent(REQUESTS_USER_AGENT.to_owned()))
+        .header(default_user_agent())
         .send()
         .map(Response::from)
 }
@@ -24,7 +28,7 @@ pub fn post(url: &str) -> Result<Response> {
 pub fn put(url: &str) -> Result<Response> {
     hyper::Client::new()
         .put(url)
-        .header(UserAgent(REQUESTS_USER_AGENT.to_owned()))
+        .header(default_user_agent())
         .send()
         .map(Response::from)
 }
@@ -32,7 +36,7 @@ pub fn put(url: &str) -> Result<Response> {
 pub fn head(url: &str) -> Result<Response> {
     hyper::Client::new()
         .head(url)
-        .header(UserAgent(REQUESTS_USER_AGENT.to_owned()))
+        .header(default_user_agent())
         .send()
         .map(Response::from)
 }
@@ -40,7 +44,7 @@ pub fn head(url: &str) -> Result<Response> {
 pub fn delete(url: &str) -> Result<Response> {
     hyper::Client::new()
         .delete(url)
-        .header(UserAgent(REQUESTS_USER_AGENT.to_owned()))
+        .header(default_user_agent())
         .send()
         .map(Response::from)
 }
